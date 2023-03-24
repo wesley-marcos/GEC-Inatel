f02aa2c0ada3ae16b3b8f12e00c46f774640241f
Wesley Marcos <wesley.marcos@gec.inatel.br>
2023-03-24 19:57:14 -0300

    inicio da criacao dos testes

---
+++ P9/C214 - Engenharia de Software/Teoria/atividade_equivalencia/src/test/java/ProfessorConst.java
@@ -0,0 +1,32 @@
+public class ProfessorConst {
+
+    public static String CHRIS =
+            "{ \"nomeDoProfessor\" : \"Christopher\", \n" +
+               "\"horarioDeAtendimento\" : \"19:30\", \n" +
+                "\"periodo\" : \"Noturno\", \n" +
+                "\"sala\" : \"18\"}";
+
+    public static String RENZO =
+            "{ \"nomeDoProfessor\" : \"Renzo\", \n" +
+                    "\"horarioDeAtendimento\" : \"17:30\", \n" +
+                    "\"periodo\" : \"Noturno\", \n" +
+                    "\"sala\" : \"18\"}";
+
+    public static String MARCELO =
+            "{ \"nomeDoProfessor\" : \"Marcelo\", \n" +
+                    "\"horarioDeAtendimento\" : \"19:30\", \n" +
+                    "\"periodo\" : \"Integral\", \n" +
+                    "\"sala\" : \"18\"}";
+
+    public static String YVO =
+            "{ \"nomeDoProfessor\" : \"Yvo\", \n" +
+                    "\"horarioDeAtendimento\" : \"17:30\", \n" +
+                    "\"periodo\" : \"Noturno\", \n" +
+                    "\"sala\" : \"14\"}";
+
+    public static String SAMUEL =
+            "{ \"nomeDoProfessor\" : \"Christopher\", \n" +
+                    "\"horarioDeAtendimento\" : \"15:30\", \n" +
+                    "\"periodo\" : \"Noturno\", \n" +
+                    "\"sala\" : \"7\"}";
+}
